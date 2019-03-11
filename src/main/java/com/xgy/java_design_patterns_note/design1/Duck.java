ee56544176b8418a3643ba4e278d78f6789baba3
justxgy <justxgy@outlook.com>
2019-03-11 17:01:57 +0800

    first commit

---
+++ src/main/java/com/xgy/java_design_patterns_note/design1/Duck.java
@@ -0,0 +1,4 @@
+package com.xgy.java_design_patterns_note.design1;
+
+public class Duck {
+}
---
+++ src/test/java/com/xgy/java_design_patterns_note/JavaDesignPatternsNoteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xgy.java_design_patterns_note;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDesignPatternsNoteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
