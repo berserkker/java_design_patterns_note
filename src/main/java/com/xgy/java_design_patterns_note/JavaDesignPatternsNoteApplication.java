912b86698b6c5b7192bfec9f9561dbe6452478b2
xgy <justxgy@outlook.com>
2019-03-11 22:00:01 +0800

    first commit

---
+++ src/main/java/com/xgy/java_design_patterns_note/JavaDesignPatternsNoteApplication.java
@@ -0,0 +1,13 @@
+package com.xgy.java_design_patterns_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignPatternsNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDesignPatternsNoteApplication.class, args);
+    }
+
+}
